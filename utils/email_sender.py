624b066ed437d31f3a7756920fdeca50e2cb5b2e
Vaishnavi Sainath Pachange <vaishnavipachange714@gmail.com>
2025-12-01 00:06:51 +0530

    Add files via upload

---
+++ utils/email_sender.py
@@ -0,0 +1,15 @@
+import resend
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+resend.api_key = os.getenv("RESEND_API_KEY", "")
+
+def send_email(to, subject, html):
+    params = {
+        "from": "AI Travel Planner <onboarding@resend.dev>",
+        "to": ['vp04@gmail.com'],
+        "subject": subject,
+        "html": html
+    }
+    return resend.Emails.send(params)
